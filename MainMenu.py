05a2f3c6d75a03e01ecb1de66a2eb614cd7616d7
pratham1286-design <pratham1286@gmail.com>
2026-07-13 14:48:54 +0530

    tried connecting two pages

--- MainMenu.py
+++ MainMenu.py
@@ -2,3 +2,5 @@ print("="*55)
 print("                 FIT TRACK PRO")
 print("         Personal Fitness Management System")
 print("="*55)
+from Profile import username
+username()
---
+++ Profile.py
@@ -0,0 +1,2 @@
+def username():
+    print("pratham")
