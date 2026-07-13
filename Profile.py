3cee1764e1c5cfd3c43f82039391524601c3873e
pratham1286-design <pratham1286@gmail.com>
2026-07-13 19:58:40 +0530

    profile features added

--- Profile.py
+++ Profile.py
@@ -1,2 +1,16 @@
-def username():
-    print("pratham")
+def userinfo():
+    Name=input("Enter Name: ")
+    Age=int(input("Enter Age: "))
+    gender=int(input("GENDER: 1 for male OR 2 for female:"))
+    if gender==1:
+        gender="Male"
+    elif gender==2:
+        gender="Female"
+    else:
+        print("invalid input")
+    print(gender)
+    Height=int(input("Height(cm): "))
+    Weight=int(input("Current Weight(kgs): "))
+    Goal_Weight=int(input("Goal Weight(kgs): "))
+
+    
