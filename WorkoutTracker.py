327423f3d5f2f642217ac3409581de89179e93be
pratham1286-design <pratham1286@gmail.com>
2026-07-14 22:59:28 +0530

    new code added to workouttracker

--- WorkoutTracker.py
+++ WorkoutTracker.py
@@ -1,9 +1,32 @@
 def workouttracker():
+
+    def beginnertemplate():
+        
+    def mediumtemplate():
+
+    def advancedtemplate():
+
     def selfworkouts():
         print("hello")
         
     def suggestedworkouts():
-        while
+        while True:
+            print("Choose your Workout Template: ")
+            print("1 - Beginner(1-6 months)")
+            print("2 - Medium(6-12 months)")
+            print("3 - Advanced(12-further months)")
+            print("4 - GoBack")
+            select=int(input("Choose the Template : "))
+            if select==1:
+                beginnertemplate()
+            elif select==2:
+                mediumtemplate()
+            elif select==3:
+                advancedtemplate()
+            elif select==4:
+                break
+            else:
+                print("Invalid Input")
         
     while True:
         print("Choose one of the number: ")
