aa93f013898a09dd48bf8918b62c4d277c7ec53f
pratham1286-design <pratham1286@gmail.com>
2026-07-14 22:36:48 +0530

    added code to workouttracker

--- WorkoutTracker.py
+++ WorkoutTracker.py
@@ -1,4 +1,23 @@
-
-
-while True:
+def workouttracker():
+    def selfworkouts():
+        print("hello")
+        
+    def suggestedworkouts():
+        while
+        
+    while True:
+        print("Choose one of the number: ")
+        print("1 - SELF WORKOUTS")
+        print("2 - SUGGESTED WORKOUTS")
+        print("3 - Exit")
+        choice=int(input("Fill Choice : "))
+        if choice==1:
+            selfworkouts()
+        elif choice==2:
+            suggestedworkouts()
+        elif choice==3:
+            break
+        else:
+            print("Invalid Input")
+        
     
