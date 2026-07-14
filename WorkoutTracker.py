068e12516746f21675a604ee5fcb435e2cb382a0
pratham1286-design <pratham1286@gmail.com>
2026-07-15 00:24:43 +0530

    working forward

--- WorkoutTracker.py
+++ WorkoutTracker.py
@@ -10,10 +10,22 @@ def workouttracker():
         print("hello")
         from datetime import date
         today=date.today()
-        import csv
-        with open("workouts.csv","a",newline="") as f:
-            writer=csv.writer(f)
-            writer.writerow([today,])
+        while True:
+            print("Choose if you want to add Exercise")
+            print("1 - ADD new Exercise")
+            print("2 - Stop adding")
+            sel=int(input("Choose what you want to do(1,2) :"))
+            if sel==1:
+                Exercise=input("Exercise: ")
+                Sets=int(input("Sets: "))
+                Reps=int(input("Reps: "))
+                Weight_Lifted=int(input("Weight Lifted: "))
+                Duration=int(input("Duration in minutes: "))
+                Calories=int(input("Calories(kcal): "))
+                import csv
+                with open("workouts.csv","a",newline="") as f:
+                    writer=csv.writer(f)
+                    writer.writerow([today,Exercise,Sets,Reps,Weight_Lifted,Duration,Calories])
         
     def suggestedworkouts():
         while True:
@@ -22,7 +34,7 @@ def workouttracker():
             print("2 - Medium(6-12 months)")
             print("3 - Advanced(12-further months)")
             print("4 - GoBack")
-            select=int(input("Choose the Template : "))
+            select=int(input("Choose the Template(1,2,3,4) : "))
             if select==1:
                 beginnertemplate()
             elif select==2:
@@ -39,7 +51,7 @@ def workouttracker():
         print("1 - SELF WORKOUTS")
         print("2 - SUGGESTED WORKOUTS")
         print("3 - Exit")
-        choice=int(input("Fill Choice : "))
+        choice=int(input("Fill Choice(1,2,3) : "))
         if choice==1:
             selfworkouts()
         elif choice==2:
