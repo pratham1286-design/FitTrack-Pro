908e5faa5bdf2dbd0a4820e4711afb39a51688c8
pratham1286-design <pratham1286@gmail.com>
2026-07-15 13:02:23 +0530

    added beginner template

--- MainMenu.py
+++ MainMenu.py
@@ -2,5 +2,5 @@ print("="*55)
 print("                 FIT TRACK PRO")
 print("         Personal Fitness Management System")
 print("="*55)
-from Profile import userinfo
-userinfo()
+from WorkoutTracker import workouttracker
+workouttracker()
--- WorkoutTracker.py
+++ WorkoutTracker.py
@@ -1,15 +1,33 @@
 def workouttracker():
-
+    from  datetime import date
+    today=date.today()
     def beginnertemplate():
-        
+        beginner_exercise:[[today,"beginner","Lat Pulldown",3,12,15,10,30],[today,"beginner","Bicep Curls",3,12,5,10,20],[today,"beginner","Tricep Bar Pushdown",3,12,15,10,20],[today,"beginner","Pushups",3,12,0,10,35],[today,"beginner","Squats",3,12,0,10,25]]
+        while True:
+            print("Choose the Week day:")
+            print("1 - Day1")
+            print("2 - Day2")
+            print("3 - Day3")
+            print("4 - Day4")
+            print("5 - Day5")
+            print("6 - Exit")
+            day=int(input("Choose the day(1,2,3,4,5,6):"))
+            if day in  [1,2,3,4,5]:
+                import csv
+                with open("workouts.csv","a",newline="") as file
+                writer=csv.writer(file)
+                writer.writerows(beginner_exercise)
+            elif day==6:
+                break
+            else:
+                print("Invalid Input")
+            
+
     def mediumtemplate():
-
-    def advancedtemplate():
-
-    def selfworkouts():
         print("hello")
-        from datetime import date
-        today=date.today()
+    def advancedtemplate():
+        print("hi")
+    def selfworkouts():
         while True:
             print("Choose if you want to add Exercise")
             print("1 - ADD new Exercise")
@@ -25,7 +43,11 @@ def workouttracker():
                 import csv
                 with open("workouts.csv","a",newline="") as f:
                     writer=csv.writer(f)
-                    writer.writerow([today,Exercise,Sets,Reps,Weight_Lifted,Duration,Calories])
+                    writer.writerow([today,"Self",Exercise,Sets,Reps,Weight_Lifted,Duration,Calories])
+            elif sel==2:
+                break
+            else:
+                print("Invalid input")
         
     def suggestedworkouts():
         while True:
