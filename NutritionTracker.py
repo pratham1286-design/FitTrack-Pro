53d436f1f3dcf8298523de9d14113553ad655707
pratham1286-design <pratham1286@gmail.com>
2026-07-15 22:17:41 +0530

    nutrition page started

---
+++ NutritionTracker.py
@@ -0,0 +1,14 @@
+def nutrition():
+    def add_food():
+        pass
+    def search_food():
+        pass
+    def delete_food():
+        pass
+    def total_calories():
+        pass
+    def marcos():
+        pass
+    def protein_guide():
+        pass
+    
