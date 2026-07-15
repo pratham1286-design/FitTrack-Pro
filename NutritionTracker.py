749278df8e72bd6e15aa25e7c5e27bea4f0efa1b
pratham1286-design <pratham1286@gmail.com>
2026-07-16 00:42:00 +0530

    adding print to nutrition

--- NutritionTracker.py
+++ NutritionTracker.py
@@ -11,4 +11,13 @@ def nutrition():
         pass
     def protein_guide():
         pass
-    
+    while True:
+        print("======NUTRITION======")
+        print("1 - Add Food")
+        print("2 - Search Food")
+        print("3 - Delete Food")
+        print("4 - Total Calories")
+        print("5 - Total Macros")
+        print("6 - Protein Guide")
+        print("7 - Exit")
+        
