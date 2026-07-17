f77e6b8e8bbbc26a4bf6b36ff3c94479bf5d5103
pratham1286-design <pratham1286@gmail.com>
2026-07-17 14:48:18 +0530

    adding features to nutrition

--- NutritionTracker.py
+++ NutritionTracker.py
@@ -1,6 +1,12 @@
 def nutrition():
+    from datetime import date
+    today=date.today()
     def add_food():
-        pass
+        while True:
+            Type=int(input("1 - Breakfast/2 - Lunch/3 - Dinner/4 - Exit: "))
+            if Type==1:
+                food=input("Add breakfast: ")
+                
     def search_food():
         pass
     def delete_food():
@@ -20,4 +26,20 @@ def nutrition():
         print("5 - Total Macros")
         print("6 - Protein Guide")
         print("7 - Exit")
-        
+        nut=int(input("Select the option(1,2,3,4,5,6,7): "))
+        if nut==1:
+            add_food()
+        elif nut==2:
+            search_food()
+        elif nut==3:
+            delete_food()
+        elif nut==4:
+            total_calories()
+        elif nut==5:
+            marcos()
+        elif nut==6:
+            protein_guide()
+        elif nut==7:
+            break
+        else:
+            print("Invalid Input")
