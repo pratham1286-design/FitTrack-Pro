d9a1afe8697dc9ef644c97e3105f2433c93cbb69
pratham1286-design <pratham1286@gmail.com>
2026-07-20 02:07:34 +0530

    fixing some issues

--- Profile.py
+++ Profile.py
@@ -12,7 +12,6 @@ def userinfo():
     Height=int(input("Height(cm): "))
     Weight=int(input("Current Weight(kgs): "))
     Goal_Weight=int(input("Goal Weight(kgs): "))
-
     FitnessGoal=int(input("FITNESS GOAL: 1 for Lose Fat , 2 for Muscle Building , 3 for Maintaining Weight :"))
     if FitnessGoal==1:
         FitnessGoal="Lose Fat"
@@ -23,11 +22,8 @@ def userinfo():
     else:
         FitnessGoal="Muscle Building"
         print("invalid input")
-    
-
     from datetime import date
     today=date.today()
-
     import pandas as pd 
     df=pd.read_csv("userinfo.csv")
     new_row={"Date":today,"Name":Name,"Age":Age,"Gender":gender,"Height":Height,"Weight":Weight,"Goal Weight":Goal_Weight,"Fitness Goal":FitnessGoal}
