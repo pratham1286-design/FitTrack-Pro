c96fe7dd947b769ada7c56a42e83c0b7218aa30c
pratham1286-design <pratham1286@gmail.com>
2026-07-20 13:30:31 +0530

    adding the features on tools py

---
+++ Tools.py
@@ -0,0 +1,16 @@
+def Tools():
+    def bmi():
+        pass
+    def bmr():
+        pass
+    def DailyCalreq():
+        pass
+    def TDEE():
+        pass
+    def dailymacros():
+        pass
+    def waterintake():
+        pass
+    def idealweight():
+        pass
+    
