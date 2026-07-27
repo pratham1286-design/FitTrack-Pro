713fa6bd1d9b28d6a5046cfc10ef17df52612f57
pratham1286-design <pratham1286@gmail.com>
2026-07-27 22:46:57 +0530

    adding frame work to advanced template

--- WorkoutTracker.py
+++ WorkoutTracker.py
@@ -94,7 +94,69 @@ def workouttracker():
             else:
                 print("Invalid Input")
     def advancedtemplate():
+        import csv
         print("hi")
+        mediumday1=[[today,"medium","Wide Grip Lat pulldown",3,12,32.5,10,40],[today,"medium","Chest Supported T bar row",3,12,15,10,35],[today,"medium","V Grip Lat Pulldown",3,12,25,10,30],[today,"medium","Seated Cable Row",3,12,15,10,30],[today,"medium","Single Arm Dumbbell Row",3,12,7.5,10,35]]
+        mediumday2=[[today,"medium","Bicep Curls",3,12,10,10,30],[today,"medium","EZ Bar Preacher Curls",3,12,5,10,30],[today,"medium","Incline Dumbbell Curls",3,12,5,10,30],[today,"medium","Hammer Curls",3,12,5,10,30]]
+        mediumday3=[[today,"medium","Barbell Bench Press",3,15,32.5,10,40],[today,"medium","Incline Dumbbell Press",3,12,15,10,35],[today,"medium","Chest Fly",3,12,25,10,30],[today,"medium","Weighted Chest Dips",3,12,15,10,30],[today,"medium","High to Low Cable Fly",3,12,7.5,10,35]]
+        mediumday4=[[today,"medium","Cable Triceps Pushdown",3,12,10,10,30],[today,"medium","Overhead Dumbbell Tricep Extensions",3,12,5,10,30],[today,"medium","Skull Crushers",3,12,5,10,30],[today,"medium","Close Grip Bench Press",3,12,5,10,30]]
+        mediumday5=[[today,"medium","Barbell Overhead Press",3,12,32.5,10,40],[today,"medium","Dumbbell Lateral Raises",3,12,15,10,35],[today,"medium","Rear Delt Fly",3,12,25,10,30],[today,"medium","Cable Lateral Raise",3,12,15,10,30],[today,"medium","Face Pull",3,12,7.5,10,35]]
+        mediumday6=[[today,"medium","Barbell Back Squat",3,12,32.5,10,40],[today,"medium","Leg press",3,12,15,10,35],[today,"medium","Romanaian DeadLift",3,12,25,10,30],[today,"medium","Leg Extension",3,12,15,10,30],[today,"medium","Seated Calf Raises",3,12,7.5,10,35]]
+        while True:
+            print("Choose the day of the MEDIUM WORKOUT: ")
+            print("1 - Day1 [BACK DAY]")
+            print("2 - Day2 [BICEP DAY]")
+            print("3 - Day3 [CHEST DAY]")
+            print("4 - Day4 [TRICEP DAY]")
+            print("5 - Day5 [SHOULDER DAY]")
+            print("6 - Day6 [LEG DAY]")
+            print("7 - EXIT")
+            day=int(input("Choose the Day(1,2,3,4,5,6,7): "))
+            if day==1:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday1)
+                    for exercise in mediumday1:
+                        date,level,name,sets,reps,weight,duration,calories=exercise
+                        print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}, {duration}mins burning {calories}kcal")
+            elif day==2:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday2)
+                    for exercise in mediumday2:
+                        date,level,name,sets,reps,weight,duration,calories=exercise                            print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}, {duration}mins burning {calories}kcal")
+            elif day==3:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday3)
+                    for exercise in mediumday3:
+                        date,level,name,sets,reps,weight,duration,calories=exercise
+                        print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}, {duration}mins burning {calories}kcal")
+            elif day==4:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday4)
+                    for exercise in mediumday4:
+                        date,level,name,sets,reps,weight,duration,calories=exercise
+                        print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}kg, {duration}mins burning {calories}kcal")
+            elif day==5:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday5)
+                    for exercise in mediumday5:
+                        date,level,name,sets,reps,weight,duration,calories=exercise
+                        print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}kg, {duration}mins burning {calories}kcal")
+            elif day==6:
+                with open("workouts.csv","a",newline="") as file:
+                    writer=csv.writer(file)
+                    writer.writerows(mediumday6)
+                    for exercise in mediumday6:
+                                date,level,name,sets,reps,weight,duration,calories=exercise
+                                print(f"{date} : {level} - {name} : {sets}sets*{reps}reps of {weight}kg, {duration}mins burning {calories}kcal")
+                    elif day==7:
+                        break
+                    else:
+                        print("Invalid Input")
     def selfworkouts():
         while True:
             print("Choose if you want to add Exercise")
